c896432711bb2a9c29bead34ea276a593e5c52ce
kmoldobayev <logicdev1@gmail.com>
2023-08-05 20:27:57 +0600

    First commit

---
+++ src/main/java/com/codify/codifygitproject/CodifyGitProjectApplication.java
@@ -0,0 +1,13 @@
+package com.codify.codifygitproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodifyGitProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodifyGitProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codify/codifygitproject/CodifyGitProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codify.codifygitproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodifyGitProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
